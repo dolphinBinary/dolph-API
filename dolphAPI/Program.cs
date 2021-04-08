e88cf6e7cec95e89e5534bff000e1f12f4467e7c
dolphinBinary <dolphinbinary@gmail.com>
2021-04-08 22:03:43 +0300

    * Refactor input When user input num this num validates
    * Validation
    Now it's just check is it integer or not

--- dolphAPI/Program.cs
+++ dolphAPI/Program.cs
@@ -10,25 +10,41 @@ namespace dolphAPI
         {
             // N number
             Console.WriteLine("Enter number of departments: ");
-            var depNum = Console.ReadLine();
+            var depNum = Validation(Console.ReadLine());
+            Console.WriteLine(depNum);
 
             // Choose filling method
             Console.WriteLine("\nEnter number of the configuration filling method: \n 1) Manualy \n 2) Random");
-            var numMethod = Console.ReadLine();
+            var numMethod = Validation(Console.ReadLine());
+            Console.WriteLine(numMethod);
+            
+            // Configure department rules
+            int[] depRules = new int[depNum];
+            for (int i = 0; i < depNum; i++)
+            {
+                Console.WriteLine(
+                    "Enter rules: in format: number of absolute rule (or number of conditional rule + else rule * 10) \nSet absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K. \n \nElse \n \n 7) Put a new stamp T if it does not already exist (or is crossed out) or do not put any. \n 8) Cross out the existing R stamp if it already exists (or not) or do not cross out any. \n 9) Send Vasya to the next department P.");
+                var rule = Validation(Console.ReadLine());
+                Console.WriteLine(rule);
+                depRules[i] = rule;
+            }
 
-            Console.WriteLine(
-                "Enter rules: in format: number of absolute rule (or number of conditional rule + else rule * 10) \nSet absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K. \n \nElse \n \n 7) Put a new stamp T if it does not already exist (or is crossed out) or do not put any. \n 8) Cross out the existing R stamp if it already exists (or not) or do not cross out any. \n 9) Send Vasya to the next department P.");
-            var rule = Console.ReadLine();
-            
-            Console.WriteLine(Validation(depNum));
-            
+            for (int i = 0; i < depRules.Length; i++)
+            {
+                Console.Write($" {depRules[i]} ");    
+            }
         }
 
-        static bool Validation(string input)
+        static int Validation(string input)
         {
-            Regex regex = new Regex(@"^[+]?[0-7]*\.?[0-7]+$");
-            return regex.IsMatch(input);
+            int i;
+            var res = int.TryParse(input, out i);
+            if (res == false)
+            {
+                input = "Entered number is incorrect";
+            }
+            Console.WriteLine(res);
+            return Convert.ToInt32(input);
         }
-        
     }
 }
