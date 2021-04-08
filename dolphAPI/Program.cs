f50f1d4c27dd7520d49312adffee2ce1d6304661
dolphinBinary <dolphinbinary@gmail.com>
2021-04-08 22:40:46 +0300

    Refactored output

--- dolphAPI/Program.cs
+++ dolphAPI/Program.cs
@@ -9,7 +9,6 @@ namespace dolphAPI
             // N number
             Console.WriteLine("Enter number of departments: ");
             var depNum = Validation(Console.ReadLine());
-            Console.WriteLine(depNum);
 
             // Configure department rules
             int[] depRules = new int[depNum];
@@ -18,7 +17,6 @@ namespace dolphAPI
                 Console.WriteLine(
                     "Enter rules: in format: number of absolute rule \n(or number of conditional rule + else rule * 10) \nSet absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K. \n \nElse \n \n 7) Put a new stamp T if it does not already exist (or is crossed out) or do not put any. \n 8) Cross out the existing R stamp if it already exists (or not) or do not cross out any. \n 9) Send Vasya to the next department P.");
                 var rule = Validation(Console.ReadLine());
-                Console.WriteLine(rule);
                 depRules[i] = rule;
             }
 
