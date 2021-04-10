d4817c38187e899ced32d489c304f64872c3c7b1
dolphinBinary <dolphinbinary@gmail.com>
2021-04-10 13:24:59 +0300

    * Added dolphLib Temporary this lib just validate user input

--- dolphAPI/Program.cs
+++ dolphAPI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using dolphLib;
 
 namespace dolphAPI
 {
@@ -8,15 +9,16 @@ namespace dolphAPI
         {
             // N number
             Console.WriteLine("Enter number of departments: ");
-            var depNum = ParseIntOrReturn0(Console.ReadLine());
+            var depNum = Configurator.ParseIntOrReturn0(Console.ReadLine());
 
+            
+            var depRules = new int[depNum]; // Array with absolute rules
+            var rulesElse = new int[depNum]; // Array with conditional rules
             // Configure department rules
-            var depRules = new int[depNum];
-            var rulesElse = new int[depNum];
             for (int i = 0; i < depNum; i++)
             {
                 Console.WriteLine("Enter number of absolute or conditional rule \n Set absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K.");
-                var rule = ParseIntOrReturn0(Console.ReadLine());
+                var rule = Configurator.ParseIntOrReturn0(Console.ReadLine());
                 depRules[i] = rule;
                 
                 rulesElse[i] = ReturnTempRule(rule);
@@ -32,7 +34,7 @@ namespace dolphAPI
             {
                 Console.WriteLine(
                     "\n \n Input else rule \n \n 7) Put a new stamp T if it does not already exist (or is crossed out) or do not put any. \n 8) Cross out the existing R stamp if it already exists (or not) or do not cross out any. \n 9) Send Vasya to the next department P. \n");
-                var tempRule = ParseIntOrReturn0(Console.ReadLine());
+                var tempRule = Configurator.ParseIntOrReturn0(Console.ReadLine());
                 if (tempRule is > 6 and < 10)
                 {
                     return tempRule;
@@ -41,18 +43,13 @@ namespace dolphAPI
             return 0;
         }
         
-        // Parse user input or return 0 if user input is no valid 
-        static int ParseIntOrReturn0(string input)
-        {
-            return int.TryParse(input, out var parsed) && parsed >= 1 ? parsed : 0;
-        }
-        
+
         // Print Library and API output 
         private static void Printer(int[] depRules, int[] rulesElse)
-        { 
-            for (var i = 0; i < depRules.Length; i++) 
+        {
+            for (var i = 0; i < depRules.Length; i++)
             {
-                Console.Write($"| {depRules[i]} "); 
+                Console.Write($"| {depRules[i]} ");
                 Console.Write($" {rulesElse[i]} ");
             }
         }
---
+++ dolphLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dolphLib
+{
+    public static class Configurator
+    {
+        // Parse user input or return 0 if user input is no valid 
+        public static int ParseIntOrReturn0(string input)
+        {
+            return int.TryParse(input, out var parsed) && parsed >= 1 ? parsed : 0;
+        }
+    }
+}
