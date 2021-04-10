918413a0aca3241eef49db8c73f7eb809b0f3e05
dolphinBinary <dolphinbinary@gmail.com>
2021-04-10 13:50:02 +0300

    Formatted code

--- dolphAPI/Program.cs
+++ dolphAPI/Program.cs
@@ -10,7 +10,6 @@ namespace dolphAPI
             // N number
             Console.WriteLine("Enter number of departments: ");
             var depNum = Configurator.ParseIntOrReturn0(Console.ReadLine());
-
             
             var depRules = new int[depNum]; // Array with absolute rules
             var rulesElse = new int[depNum]; // Array with conditional rules
@@ -20,9 +19,7 @@ namespace dolphAPI
                 Console.WriteLine("Enter number of absolute or conditional rule \n Set absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K.");
                 var rule = Configurator.ParseIntOrReturn0(Console.ReadLine());
                 depRules[i] = rule;
-                
                 rulesElse[i] = ReturnTempRule(rule);
-                
             }
             Printer(depRules, rulesElse);
         }
@@ -43,7 +40,6 @@ namespace dolphAPI
             return 0;
         }
         
-
         // Print Library and API output 
         private static void Printer(int[] depRules, int[] rulesElse)
         {
