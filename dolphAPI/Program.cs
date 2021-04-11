0a4fdc27c8f95d4d436ece2ab291acec99d4806c
dolphinBinary <dolphinbinary@gmail.com>
2021-04-11 16:18:58 +0300

    * Created class checker which will check status of bypass end return to user information about status of bypass * Now RuleParser more stable

--- dolphAPI/Program.cs
+++ dolphAPI/Program.cs
@@ -9,7 +9,7 @@ namespace dolphAPI
         {
             // N number
             Console.WriteLine("Enter number of departments: ");
-            var depNum = Configurator.ParseIntOrReturn0(Console.ReadLine());
+            var depNum = Checker.ParseIntOrReturn0(Console.ReadLine());
             
             var depRules = new int[depNum]; // Array with absolute rules
             var rulesElse = new int[depNum]; // Array with conditional rules
@@ -17,7 +17,7 @@ namespace dolphAPI
             for (var i = 0; i < depNum; i++)
             {
                 Console.WriteLine("Enter number of absolute or conditional rule \n Set absolute rule: \n \n 1) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 2) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 3) Send Vasya to the next department K. \n \nSet conditional rule: \n If the bypass sheet has an uncrossed N stamp, then: \n 4) Put a new stamp I if it does not already exist (or it is crossed out) or do not put any. \n 5) Cross out the existing seal J if it already exists and is not crossed out, or do not cross out any seal. \n 6) Send Vasya to the next department K.");
-                var rule = Configurator.ParseIntOrReturn0(Console.ReadLine());
+                var rule = Checker.ParseIntOrReturn0(Console.ReadLine());
                 depRules[i] = ReturnAbsOrCondRule(rule);
                 rulesElse[i] = ReturnElseRule(rule);
             }
@@ -41,7 +41,7 @@ namespace dolphAPI
             {
                 Console.WriteLine(
                     "\n \n Input else rule \n \n 7) Put a new stamp T if it does not already exist (or is crossed out) or do not put any. \n 8) Cross out the existing R stamp if it already exists (or not) or do not cross out any. \n 9) Send Vasya to the next department P. \n");
-                var tempRule = Configurator.ParseIntOrReturn0(Console.ReadLine());
+                var tempRule = Checker.ParseIntOrReturn0(Console.ReadLine());
                 if (tempRule is > 6 and < 10)
                 {
                     return tempRule;
--- dolphLib/dolphLib.cs
+++ dolphLib/dolphLib.cs
@@ -17,24 +17,26 @@ namespace dolphLib
                         {
                             bypass[i] = i + 1;
                         }
-                        i = ReturnRuleElse(rulesElse, i, bypass);
+                        i = ReturnRuleElse(rulesElse, bypass, i);
                         break;
                     
                     case 2: case 5:
-                        if (bypass[i] == i + 1)
+                        if (bypass[i] >= 0)
                         {
                             bypass[i] = -1;
                         }
-                        i = ReturnRuleElse(rulesElse, i, bypass);
+                        i = ReturnRuleElse(rulesElse, bypass, i);
                         break;
                     
                     case 3: case 6:
-                        Console.WriteLine("\n Differences in bypass: ");
-                        i = ReturnRuleElse(rulesElse, i, bypass);
+                        Console.WriteLine("\n Enter goto department parameter");
+                        var tempEnum = Checker.ParseIntOrReturn0(Console.ReadLine());
+                        i = rulesElse[i] < 7 ? ReturnParameterOrIterator(tempEnum, depRules) : ReturnRuleElse(rulesElse, bypass, i);
                         break;
                 }
             }
-
+            
+            Console.WriteLine("\n Differences in bypass: ");
             foreach (var t in bypass)
             {
                 Console.Write($"| {t} ");
@@ -42,35 +44,48 @@ namespace dolphLib
             return bypass;
         }
 
-        private static int ReturnRuleElse(int[] rulesElse, int i, int[] bypass)
+        private static int ReturnRuleElse(int[] rulesElse, int[] bypass, int i)
         {
             switch (rulesElse[i])
             {
                 case 7:
                     if (bypass[i] is 0)
                     {
-                        bypass[i] = i + 1;
+                        Console.WriteLine("\n Enter stamp number");
+                        var tempStamp = Checker.ParseIntOrReturn0(Console.ReadLine());
+                        bypass[ReturnParameterOrIterator(tempStamp, rulesElse)] = ReturnParameterOrIterator(tempStamp, rulesElse);
                     }
                     break;
+                
                 case 8:
+                    Console.WriteLine("\n Enter department parameter");
+                    var tempDep = Checker.ParseIntOrReturn0(Console.ReadLine());
                     if (bypass[i] == i + 1)
                     {
-                        bypass[i] = -1;
+                        bypass[ReturnParameterOrIterator(tempDep, rulesElse)] = ReturnParameterOrIterator(tempDep, rulesElse);
                     }
                     break;
+                
                 case 9:
-                    Console.WriteLine("\n Enter goto parameter");
-                    var tempEnum = ParseIntOrReturn0(Console.ReadLine());
-                    if (tempEnum < rulesElse.Length)
-                    {
-                        return tempEnum;
-                    }
+                    Console.WriteLine("\n Enter goto department parameter");
+                    var tempEnum = Checker.ParseIntOrReturn0(Console.ReadLine());
+                    bypass[ReturnParameterOrIterator(tempEnum, rulesElse)] = ReturnParameterOrIterator(tempEnum, rulesElse);
                     break;
             }
             return i;
         }
+
+        private static int ReturnParameterOrIterator(int tempParam, int[] rules)
+        {
+            return tempParam <= rules.Length ? tempParam : 0;
+        }
         
         // Parse user input or return 0 if user input is no valid 
+        
+    }
+
+    public static class Checker
+    {
         public static int ParseIntOrReturn0(string input)
         {
             return int.TryParse(input, out var parsed) && parsed >= 1 ? parsed : 0;
